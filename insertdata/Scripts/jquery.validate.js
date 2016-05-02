75b94d6cc20591cd5f9671d537b869e69ed423d7
SSVasani <saurabh.vyomwork@gmail.com>
2016-05-02 17:42:16 +0530

    commitjs change

--- insertdata/Scripts/jquery.validate.js
+++ insertdata/Scripts/jquery.validate.js
@@ -11,6 +11,18 @@
  * under the terms set out in this paragraph. All notices and licenses
  * below are for informational purposes only.
  *
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  * NUGET: END LICENSE TEXT */
 /*!
  * jQuery Validation Plugin 1.11.1
